716746bac2b11ea2eab85a8482ee83c717a7e75a
Sanju2op <120462833+Sanju2op@users.noreply.github.com>
2025-05-08 10:27:38 +0530

    "backup patch"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
-// src/App.js
+// src/App.jsx
 import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
 import SignInPage from "./pages/SignInPage";
@@ -12,30 +17,43 @@ import NotFound from "./pages/NotFound";
 import EmbedSpace from "./pages/EmbedSpace";
 import EmbedTestimonial from "./pages/EmbedTestimonial";
 
-
 const clerkFrontendApi = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
-function App() {
-  return (
-    <ClerkProvider publishableKey={clerkFrontendApi}>
-      <Router>
-        <SyncUser />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup/*" element={<SignUpPage />} />
-          <Route path="/products/:spaceName" element={<ProductPage />} />
-          <Route path="/embed/space/:slug" element={<EmbedSpace />} />
-          <Route path="/embed/testimonial/:id" element={<EmbedTestimonial />} />
+function AppRoutes() {
+  const location = useLocation();
+  const isEmbedRoute = location.pathname.startsWith("/embed");
 
-          <Route path="/:slug" element={<SpacePage />} />
-          {/* 404 fallback */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+  return (
+    <>
+      {!isEmbedRoute && <SyncUser />}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup/*" element={<SignUpPage />} />
+        <Route path="/products/:spaceName" element={<ProductPage />} />
+        <Route path="/embed/space/:slug" element={<EmbedSpace />} />
+        <Route path="/embed/testimonial/:id" element={<EmbedTestimonial />} />
+        <Route path="/:slug" element={<SpacePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+function AppWrapper() {
+  const isEmbedRoute = window.location.pathname.startsWith("/embed");
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkFrontendApi}
+      {...(isEmbedRoute && { loadUser: false })}
+    >
+      <Router>
+        <AppRoutes />
       </Router>
     </ClerkProvider>
   );
 }
 
-export default App;
+export default AppWrapper;
